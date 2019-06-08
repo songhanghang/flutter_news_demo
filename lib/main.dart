2e8f7dca75a37998dd4c9085abdfb56288e41b8c
songhang <songhang@xiaomi.com>
2019-06-08 22:43:42 +0800

    [add] 下拉刷新

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
-import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'dart:async';
 
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+//  debugPaintSizeEnabled = true;
+//  debugPaintPointersEnabled = true;
+//  debugPaintLayerBordersEnabled = true;
+//  debugRepaintRainbowEnabled = true;
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -9,113 +17,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
       routes: {"new_page": (context) => new NewRoute()},
-      home: MyHomePage(title: 'Flutter Demo Home NEWS Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-            FlatButton(
-              child: Text("打开一个新界面"),
-              textColor: Colors.red,
-              onPressed: () {
-                Navigator.pushNamed(context, "new_page");
-              },
-            ),
-            RandomWordsWidget(),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: ScrollableTabsDemo(),
+      debugShowCheckedModeBanner: true,
+      debugShowMaterialGrid: false,
     );
   }
 }
@@ -135,14 +40,176 @@ class NewRoute extends StatelessWidget {
   }
 }
 
-class RandomWordsWidget extends StatelessWidget {
+class _Page {
+  const _Page({this.text, this.url});
+
+  final String text;
+  final String url;
+}
+
+const List<_Page> _allPages = <_Page>[
+  _Page(text: '新闻'),
+  _Page(text: '娱乐'),
+  _Page(text: '体育'),
+  _Page(text: '财经'),
+  _Page(text: '军事'),
+  _Page(text: '科技'),
+  _Page(text: '手机'),
+  _Page(text: '数码'),
+  _Page(text: '时尚'),
+  _Page(text: '游戏'),
+  _Page(text: '体育'),
+  _Page(text: '健康'),
+  _Page(text: '旅游'),
+  _Page(text: '视频'),
+];
+
+class ScrollableTabsDemo extends StatefulWidget {
+  static const String routeName = '/material/scrollable-tabs';
+
+  @override
+  ScrollableTabsDemoState createState() => ScrollableTabsDemoState();
+}
+
+class ScrollableTabsDemoState extends State<ScrollableTabsDemo>
+    with SingleTickerProviderStateMixin {
+  TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(vsync: this, length: _allPages.length);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Decoration _getIndicator() {
+    return ShapeDecoration(
+      shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            side: BorderSide(
+              color: Colors.white70,
+              width: 1.5,
+            ),
+          ) +
+          const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4.0)),
+            side: BorderSide(
+              color: Colors.transparent,
+              width: 8.0,
+            ),
+          ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    final wordPair = WordPair.random();
-    return Padding(
-      padding: const EdgeInsets.all(9),
-      child: Text(wordPair.toString()),
+    return Scaffold(
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverOverlapAbsorber(
+              handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+              child: SliverAppBar(
+                title: const Text('News'),
+                pinned: false,
+                backgroundColor: Colors.redAccent,
+                forceElevated: innerBoxIsScrolled,
+                bottom: TabBar(
+                  controller: _controller,
+                  isScrollable: true,
+                  labelColor: Colors.white,
+                  indicator: _getIndicator(),
+                  tabs: _allPages.map<Tab>((_Page page) {
+                    return Tab(text: page.text);
+                  }).toList(),
+                ),
+              ),
+            ),
+          ];
+        },
+        body: TabBarView(
+          controller: _controller,
+          children: _allPages.map<Widget>((_Page page) {
+            return SafeArea(
+              top: false,
+              bottom: true,
+              child: _PageWidget(page),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class _PageWidget extends StatefulWidget {
+  const _PageWidget(this.page, {Key key}) : super(key: key);
+
+  final _Page page;
+
+  @override
+  State<StatefulWidget> createState() {
+    print("createState " + page.text);
+    return _PageWidgetState().._refresh();
+  }
+}
+
+class _PageWidgetState extends State<_PageWidget> {
+  static final List<String> _items = <String>[
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+  ];
+
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+      GlobalKey<RefreshIndicatorState>();
+
+  Future<void> _refresh() {
+    final Completer<void> completer = Completer<void>();
+    Timer(const Duration(seconds: 3), () {
+      print("hadRequest = true");
+      completer.complete();
+    });
+    return completer.future.then<void>((_) {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RefreshIndicator(
+      key: _refreshIndicatorKey,
+      onRefresh: _refresh,
+      child: Scrollbar(
+        child: ListView.builder(
+          padding: kMaterialListPadding,
+          itemCount: _items.length,
+          itemBuilder: (BuildContext context, int index) {
+            final String item = _items[index];
+            return ListTile(
+              isThreeLine: true,
+              leading: CircleAvatar(child: Text(item)),
+              title: Text('This item represents $item.'),
+              subtitle: const Text(
+                  'Even more additional list item information appears on line three.'),
+            );
+          },
+        ),
+      ),
     );
   }
 }
